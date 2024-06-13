c22e9df0a8b0753778ea0fdd7d2cef5274a97f68
BryceKopan <BryceKopan@gmail.com>
2024-06-13 14:41:48 -0500

    Day 3, Part 2, Incomplete, 1 bug(getPartNumber() not returning correctly when number is at index 0)

--- 2023/Day-3.py
+++ 2023/Day-3.py
@@ -65,14 +65,8 @@ def testSumPartNumbers():
     return sumPartNumbers(testEngineSchematic) == testAnswer
 
 
-if __name__ == "__main__":
-    print("Checking for Errors ...")
-    if testSumPartNumbers():
-        print("... No Errors Found")
-    else:
-        print("... Error Found")
-        exit(1)
-
+# Part 2
+def getEngineSchematic():
     engineSchematicPath = input("Enter the file path for the .txt game data:")
     engineSchematicData = open(engineSchematicPath, 'r')
 
@@ -82,4 +76,117 @@ if __name__ == "__main__":
 
     engineSchematicData.close()
 
-    print(sumPartNumbers(engineSchematic))
+    return engineSchematic
+
+
+def isGear(lineNumber, index, nEngineSchematic):
+    adjacentNumbers = {}
+    startAdjacentRange = index
+    endAdjacentRange = index
+    if startAdjacentRange > 0:
+        startAdjacentRange -= 1
+    if endAdjacentRange != len(nEngineSchematic[lineNumber]) - 1:
+        endAdjacentRange += 1
+
+    # possible bug (because I'm storing adjacent numbers in dict if two different but identical part numbers border the same gear it will fail)
+    # I'm using dict because a single part number bordering a gear at n places will trigger the following logic n times
+    for i in range(startAdjacentRange, endAdjacentRange + 1):
+        if nEngineSchematic[lineNumber][i].isnumeric():
+            adjacentNumbers[getPartNumber(lineNumber, i, nEngineSchematic)] = 1
+        elif lineNumber > 0 and nEngineSchematic[lineNumber - 1][i].isnumeric():
+            adjacentNumbers[getPartNumber(lineNumber - 1, i, nEngineSchematic)] = 1
+        elif lineNumber < len(nEngineSchematic) - 1 and nEngineSchematic[lineNumber + 1][i].isnumeric():
+            adjacentNumbers[getPartNumber(lineNumber + 1, i, nEngineSchematic)] = 1
+
+    return len(adjacentNumbers) == 2, list(adjacentNumbers)
+
+
+def getNextGear(currentLine, currentIndex, nEngineSchematic):
+    for l in range(currentLine, len(nEngineSchematic)):
+        for i in range(currentIndex, len(nEngineSchematic[l])):
+            if nEngineSchematic[l][i] == '*':
+                bordersTwoNumbers, partNumbers = isGear(l, i, nEngineSchematic)
+                if bordersTwoNumbers:
+                    return l, i, partNumbers
+
+    return None, None, None
+
+
+def getPartNumber(lineNumber, index, nEngineSchematic):
+    partNumber = nEngineSchematic[lineNumber][index]
+
+    # Check chars to the left
+    i = 1
+    foundNonNumeric = index == 0
+    while not foundNonNumeric:
+        if index - i == 0 or not nEngineSchematic[lineNumber][index - i].isnumeric():
+            foundNonNumeric = True
+        else:
+            partNumber = nEngineSchematic[lineNumber][index - i] + partNumber
+            i += 1
+
+    # Check chars to the right
+    i = 1
+    foundNonNumeric = index == len(nEngineSchematic[lineNumber]) - 1
+    while not foundNonNumeric:
+        if index + i == len(nEngineSchematic[lineNumber]) - 1 or not nEngineSchematic[lineNumber][index + i].isnumeric():
+            foundNonNumeric = True
+        else:
+            partNumber = partNumber + nEngineSchematic[lineNumber][index + i]
+            i += 1
+
+    return int(partNumber)
+
+
+def sumGearRatio(nEngineSchematic):
+    gearRatioSum = 0
+    lineIndex = 0
+    gearIndex = 0
+
+    while lineIndex is not None and gearIndex is not None:
+        lineIndex, gearIndex, adjacentPartNumbers = getNextGear(lineIndex, gearIndex, nEngineSchematic)
+        print("Gear at:" + str(lineIndex) + ':' + str(gearIndex))
+
+        if adjacentPartNumbers is not None:
+            gearRatio = adjacentPartNumbers[0] * adjacentPartNumbers[1]
+            print(adjacentPartNumbers)
+            gearRatioSum += gearRatio
+            gearIndex += 1
+
+    return gearRatioSum
+
+
+def testSumGearRatio():
+    testEngineSchematic = [['4', '6', '7', '.', '.', '1', '1', '4', '.', '.'],
+                           ['.', '.', '.', '*', '.', '.', '.', '.', '.', '.'],
+                           ['.', '.', '3', '5', '.', '.', '6', '3', '3', '.'],
+                           ['.', '.', '.', '.', '.', '.', '#', '.', '.', '.'],
+                           ['6', '1', '7', '*', '.', '.', '.', '.', '7', '2'],
+                           ['.', '.', '.', '.', '.', '+', '.', '5', '8', '.'],
+                           ['.', '.', '5', '9', '2', '.', '.', '.', '.', '.'],
+                           ['.', '.', '.', '.', '.', '.', '7', '5', '5', '.'],
+                           ['.', '.', '.', '$', '.', '*', '.', '.', '.', '.'],
+                           ['.', '6', '6', '4', '.', '5', '9', '8', '.', '.']]
+    testAnswer = 467835
+
+    testResult = sumGearRatio(testEngineSchematic)
+    print(testResult)
+    return testResult == testAnswer
+
+if __name__ == "__main__":
+    print("Checking for Errors ...")
+    if testSumPartNumbers() and testSumGearRatio():
+        print("... No Errors Found")
+    else:
+        print("... Error Found")
+        exit(1)
+
+    currentEngineSchematic = getEngineSchematic()
+
+    desiredSum = int(input("Choose 1 or 2.\n 1: Engine Part Sum\n 2: Gear Ratio Sum\n"))
+    if desiredSum == 1:
+        print(sumPartNumbers(currentEngineSchematic))
+    elif desiredSum == 2:
+        print(sumGearRatio(currentEngineSchematic))
+    else:
+        print("Invalid Choice")
