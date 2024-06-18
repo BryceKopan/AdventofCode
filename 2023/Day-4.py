b6ea950a6899243dafc5391bbf7fa1eb07e260d6
BryceKopan <BryceKopan@gmail.com>
2024-06-18 14:30:14 -0500

    Day 4, Part 1, Complete

--- 2023/Day-4.py
+++ 2023/Day-4.py
@@ -6,13 +6,14 @@ def unitTests():
         print("... Error Found")
         exit(1)
 
+
 def testGetCardPointValue():
     testCardValues = ['Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53',
-                        'Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19',
-                        'Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1',
-                        'Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83',
-                        'Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36',
-                        'Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11']
+                      'Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19',
+                      'Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1',
+                      'Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83',
+                      'Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36',
+                      'Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11']
     testAnswer = 13
 
     testSum = 0
@@ -28,24 +29,27 @@ def getFilePath():
 
 
 def getCardPointValue(line):
-    cardValue = 0
     winningNumbers, pickedNumbers = getCardNumbers(line)
 
-    numberOfWinningNumbers = 0
+    pointValue = 0
     for number in pickedNumbers:
         if number in winningNumbers:
-            numberOfWinningNumbers += 1
+            if pointValue == 0:
+                pointValue = 1
+            else:
+                pointValue = pointValue * 2
 
-    cardValue = pow(numberOfWinningNumbers, 2)
-    return cardValue
+    return pointValue
 
 
 def getCardNumbers(line):
     line = line.strip()
     gameIdentifier, numbers = line.split(':')
     winningNumbers, chosenNumbers = numbers.split('|')
+
     winningNumbersList = getNumberList(winningNumbers)
     chosenNumbersList = getNumberList(chosenNumbers)
+
     return winningNumbersList, chosenNumbersList
 
 
